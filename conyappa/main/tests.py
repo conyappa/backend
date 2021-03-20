7343d643d3c974cbba24498098b237ef5094e2e5
Ariel Martínez Silberstein <amartinez5@uc.cl>
2021-03-20 12:39:42 -0300

    chore: add a make command to run tests

--- conyappa/main/tests.py
+++ conyappa/main/tests.py
@@ -2,5 +2,11 @@ from django.test import TestCase
 
 
 class MetaTestCase(TestCase):
-    def test_tests(self):
-        self.assertTrue(True)
+    def setUp(self):
+        self.a_true_statement = True
+
+    def test_assert_true(self):
+        """
+        Tests are working correctly.
+        """
+        self.assertTrue(self.a_true_statement)
