06d32ac9c7a69737347207da58f65a57ead5f1de
Ariel Martínez Silberstein <amartinez5@uc.cl>
2021-03-25 12:59:35 -0300

    Validate RUT and password upon sign up (#19)
    * feat: add a password validator
    
    * feat: validate correctness of rut and check digit
    
    * docs: update check digit description
    
    * style: add variable for password min length

--- conyappa/accounts/serializers.py
+++ conyappa/accounts/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, ValidationError
 from rest_framework_simplejwt.serializers import TokenObtainSlidingSerializer
 from utils.serializers import SetOnlyFieldsMixin
 
@@ -44,3 +44,60 @@ class UserSerializer(SetOnlyFieldsMixin, ModelSerializer):
             "balance": {"read_only": True},
             "winnings": {"read_only": True},
         }
+
+    @staticmethod
+    def expected_check_digit(rut):
+        remaining_digits = rut
+
+        m = 0
+        s = 1
+
+        while remaining_digits > 0:
+            last_digit = remaining_digits % 10
+            remaining_digits //= 10
+
+            coef = 9 - (m % 6)
+            m += 1
+
+            s += last_digit * coef
+            s %= 11
+
+        return (s - 1) if (s > 0) else 10
+
+    def get_field_verbose_name(self, field_name):
+        model = self.Meta.model
+        return model._meta.get_field(field_name).verbose_name
+
+    def validate_rut(self, value):
+        check_digit_name = "check_digit"
+        check_digit = self.initial_data.get(check_digit_name)
+        check_digit_verbose_name = self.get_field_verbose_name(check_digit_name)
+
+        if check_digit is None:
+            raise ValidationError(f"Don't input this field without a {check_digit_verbose_name}.")
+
+        return value
+
+    def validate_check_digit(self, value):
+        rut_name = "rut"
+        rut = self.initial_data.get(rut_name)
+        rut_verbose_name = self.get_field_verbose_name(rut_name)
+
+        if rut is None:
+            raise ValidationError(f"Don't input this field without a {rut_verbose_name}.")
+
+        if value > 10:
+            raise ValidationError("Ensure this value is less than or equal to 10.")
+
+        if value != self.expected_check_digit(rut):
+            raise ValidationError(f"Ensure this value corresponds to the inputted {rut_verbose_name}.")
+
+        return value
+
+    def validate_password(self, value):
+        PASSWORD_MIN_LENGTH = 6
+
+        if len(value) < PASSWORD_MIN_LENGTH:
+            raise ValidationError(f"Ensure this field has no less than {PASSWORD_MIN_LENGTH} characters.")
+
+        return value
