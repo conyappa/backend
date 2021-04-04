37f053d145a3d2aa4d61555713df5a038a80d5fa
Ariel Martínez Silberstein <amartinez5@uc.cl>
2021-04-04 16:19:59 -0400

    feat: add logs

--- conyappa/utils/random.py
+++ conyappa/utils/random.py
@@ -1,8 +1,10 @@
 import random as random_
 
 from django.conf import settings
-
+from logging import getLogger
 from utils import aws
+
+logger = getLogger(__name__)
 
 
 class _container:
@@ -25,10 +27,17 @@ def update():
         random_.seed(a)
         _container.value = random_
 
-    except Exception:
+        logger.info(f"Random generator seed successfully updated to '{a}'.")
+
+    except Exception as e:
         # An alternative random generator that uses os.urandom.
         _container.value = random_.SystemRandom()
 
+        logger.warning(f"Using SystemRandom as the random generator. Failed to update seed: {e}")
+
 
 def get():
-    return _container.value
+    try:
+        return _container.value
+    except AttributeError as e:
+        raise AttributeError("The random generator must be updated before it can be retrieved.") from e
