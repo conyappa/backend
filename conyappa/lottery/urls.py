62aafda4090622fa1043fca06840a392c7a5f3e9
Ariel Martínez Silberstein <amartinez5@uc.cl>
2021-04-08 10:27:31 -0400

    style: fix typo

--- conyappa/lottery/urls.py
+++ conyappa/lottery/urls.py
@@ -12,7 +12,7 @@ app_name = "lottery"
 
 urlpatterns = [
     path("draws", DrawListView.as_view(), name="draw-list"),
-    path("draws/metadata", metadata, name="draw-list"),
+    path("draws/metadata", metadata, name="draws-metadata"),
     path("draws/ongoing", OngoingDrawView.as_view(), name="ongoing-draw"),
     path("draws/ongoing/choose", choose_result, name="choose-result"),
     path("users/<uuid:user_id>/tickets", UserTicketsView.as_view(), name="user-tickets"),
