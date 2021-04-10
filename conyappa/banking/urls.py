9b8da758dbd7ef8f874c807609fb096ad7610437
Ariel Martínez Silberstein <amartinez5@uc.cl>
2021-04-10 14:06:53 -0400

    fix: typo

--- conyappa/banking/urls.py
+++ conyappa/banking/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from .views import fetch_movements
 
-app_name = "lottery"
+app_name = "banking"
 
 urlpatterns = [
     path("movements/fetch", fetch_movements, name="fetch-movements"),
